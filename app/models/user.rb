38d5bc052873d5c10f43f331ab4ef221b796b62d
Pez Cuckow <email@pezcuckow.com>
2012-02-17 17:13:38 +0000

    Fixed Pete's fail ^_^

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,5 @@ class User < ActiveRecord::Base
   include Clearance::User
   belongs_to :organisation
   
-  attr_accessor :first_name, :last_name, :email, :password
+  attr_accessible :first_name, :last_name, :email, :password
 end
