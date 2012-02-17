5cf57b381ba6080c7a60e721140cb0518240e8d6
Peter Hamilton <peter@inspiredpixel.net>
2012-02-17 13:27:48 +0000

    Initial commit - suspended project
    Created rails project with suspenders

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GamePlan::Application.config.secret_token = '1af2c0daa39eb338dac90ce4c29f9ec6a81317bdca7a688be210ee8949eb5abdd4a6dff887cfc2f340ca4241d0ef3517ebe44f592051cbd8f5b510ad3ce3d5cf'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+GamePlan::Application.config.session_store :cookie_store, key: '_GamePlan_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# GamePlan::Application.config.session_store :active_record_store
